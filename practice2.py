028a0359a5f6185cd63701f25da6974929368887
gycha0109-beep <gycha0109@gmail.com>
2026-03-24 13:59:30 +0900

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,29 @@
+lang = 'PYTHON'
+print(lang) # PYTHON
+print(lang[0]) # P 인덱스 0 (0부터 시작)
+print(lang[-1]) # N 인덱스 -1 (끝에서 1번째)
+print(lang[:3]) # PYT 인덱스 0부터 2까지
+print(lang[2:]) # THON 인덱스 2부터 끝까지
+print(lang[:]) # PYTHON 인덱스 0부터 끝까지
+
+num = 3
+num += 2 # num = num + 2
+print(num) # 5
+
+num -= 1 # 4 
+num *= 2 # 8
+num /= 4 # 2.0
+print(num) # 2.0
+
+snack = '꿀꽈배기'
+print(len(snack)) # 4 = 문자열의 길이
+
+snack = '''꿀꽈배기는
+너무
+맛있어요'''
+print(snack)
+
+print('-' * 10) # -를 10번 출력하겠다
+print('Nadocoding')
+print('*' * 20) # *를 20번 출력하겠다
+
