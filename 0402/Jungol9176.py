d912fac5e42af106b65186fbc156cb49d7025647
gycha0109-beep <gycha0109@gmail.com>
2026-04-02 19:00:01 +0900

    Add files via upload

---
+++ 0402/Jungol9176.py
@@ -0,0 +1,11 @@
+A = []
+for _ in range(2):
+    A.append(input())
+
+print(f"Capital of {A[0]} is {A[1]}")
+
+'''
+country = input().strip()
+capital = input().strip()
+print(f"Capital of {country} is {capital}")
+'''
---
+++ 0402/Jungol9262.py
@@ -0,0 +1,14 @@
+N = int(input())
+for i in range(5, N+1):
+    print(i)
+
+'''
+N = int(input())
+comp = 5
+
+while True: 
+    print(comp)
+    if comp == N:
+        break # 무한루프 탈출 조건
+    comp += 1
+'''
